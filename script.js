3b6d7a13bc3a6413e43750826065ff11cb1714d5
James Hawker <526rserbinenko@frhsd.com>
2025-06-04 12:20:21 +0000

    autonomous self-building stage 1 complete

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Game {
         score1: 0,
         score2: 0,
       });
-    this.glyphs = {
+    (this.glyphs = {
       mid: "ǁ",
       tbEdge: "—",
       ball: "█",
@@ -23,40 +23,63 @@ class Game {
              |--`,
       controller: "0",
       fill: "a",
-    };
+    }),
+      (this.grid = []);
   }
-  testGrid(width, height) {
-    const cont = DOC.get("#testGrid");
-    this.grid = Array.from({ length: height }, () =>
-      new Array(width).fill(this.glyphs.fill)
+  init(width, height) {
+    const self = this;
+    this.grid = new Proxy(this.grid, {
+      set(t, p, r) {
+        if (this.i == height - 1) {
+          alert("set");
+          self.render();
+        }
+        this.i++;
+        return Reflect.set(t, p, r);
+      },
+      isSet: false,
+      i: 0,
+    });
+    this.grid.push(
+      ...Array.from({ length: height }, () =>
+        new Array(width).fill(this.glyphs.fill)
+      )
     );
+    // this.grid.forEach((e) => {
+    //   e.forEach((a) => cont.insertAdjacentHTML("beforeend", a));
+    //   cont.insertAdjacentHTML("beforeend", "<br>");
+    // });
+    // for (let i = 0; i < this.grid.length; i++) {
+    //   this.grid[i] = new Proxy(this.grid[i], {
+    //     get(t, p, r) {
+    //       console.log("got subarray", t, p, r);
+    //       return Reflect.get(t, p, r);
+    //     },
+    //     set(t, p, r) {
+    //       console.log("set subarray", t, p, r);
+    //       return Reflect.set(t, p, r);
+    //     },
+    //   });
+    // }
+    // this.grid = new Proxy(this.grid, {
+    //   get(t, p, r) {
+    //     console.log("got lobby", t, p, r);
+    //     return Reflect.get(t, p, r);
+    //   },
+    //   set(t, p, r) {
+    //     console.log("set lobby", t, p, r);
+    //     return Reflect.set(t, p, r);
+    //   },
+    // });
+    // this.grid[0][1] = 2;
+  }
+  render() {
+    alert("here");
+    const cont = DOC.get("#testGrid");
     this.grid.forEach((e) => {
       e.forEach((a) => cont.insertAdjacentHTML("beforeend", a));
       cont.insertAdjacentHTML("beforeend", "<br>");
     });
-    for (let i = 0; i < this.grid.length; i++) {
-      this.grid[i] = new Proxy(this.grid[i], {
-        get(t, p, r) {
-          console.log("got subarray", t, p, r);
-          return Reflect.get(t, p, r);
-        },
-        set(t, p, r) {
-          console.log("set subarray", t, p, r);
-          return Reflect.set(t, p, r);
-        },
-      });
-    }
-    this.grid = new Proxy(this.grid, {
-      get(t, p, r) {
-        console.log("got lobby", t, p, r);
-        return Reflect.get(t, p, r);
-      },
-      set(t, p, r) {
-        console.log("set lobby", t, p, r);
-        return Reflect.set(t, p, r);
-      },
-    });
-    this.grid[0][1] = 2;
   }
 }
 const DOC = {
@@ -65,4 +88,4 @@ const DOC = {
   },
 };
 let game = new Game(2, 5, 2, false);
-game.testGrid(10, 10);
+game.init(10, 10);
