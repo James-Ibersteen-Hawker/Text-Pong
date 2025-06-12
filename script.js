e5bfa493b425bdec1b4513cbb2d578fc46aa2863
James Hawker <526rserbinenko@frhsd.com>
2025-06-12 15:55:03 +0000

    gridding more

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ class Game {
   maxScore;
   roundCount;
   bot;
-  constructor(res, maxScore, roundCount, bot) {
+  destination;
+  constructor(res, maxScore, roundCount, bot, destination) {
     (this.res = res),
       (this.maxScore = maxScore),
       (this.roundCount = roundCount),
@@ -11,12 +12,13 @@ class Game {
       (this.scores = {
         score1: 0,
         score2: 0,
-      });
+      }),
+      this.destination = destination,
     (this.glyphs = {
       mid: "ǁ",
-      tbEdge: "—",
+      tbEdge: "-",
       ball: "█",
-      sides: "|",
+      sides: ":",
       corner: "+",
       goal: `|--
              |
@@ -26,14 +28,18 @@ class Game {
     }),
       (this.grid = []),
       (this.ref = []);
+      this.Map();
   }
-  Map(loc) {
+  Map() {
     const self = this;
-    this.grid = Array.from({ length: 6 * this.res }, () => {
-      let arr = new Array(5 * this.res * 2).fill(this.glyphs.fill);
+    const w = 5, h = 6;
+    this.grid = Array.from({ length: h * this.res + 2 }, () => {
+      let arr = new Array(w * this.res * 2 + 1).fill(this.glyphs.fill);
       return new Proxy(arr, {
         set(t, p, r) {
-          return Reflect.set(t, p, r);
+          let reflect = Reflect.set(t, p, r);
+          self.grid.render()
+          return reflect;
         },
         get(t, p, r) {
           if (p == "self") return this.target;
@@ -55,17 +61,27 @@ class Game {
       let [k, q] = self.ref[i].split("_").map((v) => Number(v));
       return this[k][q];
     };
-    this.grid.render = function(loc) {
+    this.grid.render = function() {
+      self.destination.innerHTML = "";
       this.forEach(e => {
         e.forEach(a => {
-          loc.append(a);
+          self.destination.append(a);
         })
-        loc.insertAdjacentHTML("beforeend", "<br>");
+        self.destination.insertAdjacentHTML("beforeend", "<br>");
       })
-    }
+    };
+    //
+    this.grid[0].fill(this.glyphs.tbEdge);
+    this.grid.at(-1).fill(this.glyphs.tbEdge)
+    this.grid.forEach(e => {
+      e.forEach((a,i, arr) => {
+        if (i == w * self.res + 1) e.splice(i, 0,self.glyphs.mid);
+        else if (i == 0) e.splice(i,1, this.glyphs.sides);
+      })
+      e.push(self.glyphs.sides)
+    })
   }
 }
 
-let game = new Game(1, 10, 5, false);
-game.Map();
-game.grid.render(document.body)
+let game = new Game(2, 10, 5, false,document.querySelector("#testGrid"));
+game.grid.render()
